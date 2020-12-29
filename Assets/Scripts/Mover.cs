73b99f8070f725d0f2755704e54739c8e76701e4
mcgranpa <fmcmeans@gmail.com>
2020-12-28 20:28:06 -0600

    Testing_Raycast

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,9 +6,15 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+    Ray lastRay;
        
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
