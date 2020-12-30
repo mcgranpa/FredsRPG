b591906a1b41de03f007f14d5776b38b58726a6e
mcgranpa <fmcmeans@gmail.com>
2020-12-29 21:39:07 -0600

    added character movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,13 +5,15 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
+    bool down;
     //[SerializeField] Transform target;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoverToCursor();
         }
+        UpdateAnimator();
     }
     private void MoverToCursor()
     {
@@ -25,6 +27,16 @@ public class Mover : MonoBehaviour
         }
     }
 
+    private void UpdateAnimator()
+    {
+        // check out global velocity vs local velocity
+        // converts global to local 
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+    
     // Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
 
 }
