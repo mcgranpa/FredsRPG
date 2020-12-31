d3b5ae3ca6f5ed28a807490399aa267141ba9ff7
mcgranpa <fmcmeans@gmail.com>
2020-12-31 15:54:46 -0600

    add namespaces
    Also split out Controller and mover on player

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoverToCursor();
+            }
+        }
+
+        private void MoverToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            bool hasHit;
+            hasHit = Physics.Raycast(ray, out hitInfo);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hitInfo.point);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        bool down;
+        //[SerializeField] Transform target;
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+        private void UpdateAnimator()
+        {
+            // check out global velocity vs local velocity
+            // converts global to local 
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+        // Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
+    }
+}
