a57e22f07a0af839bf6b8ea34504412d46071f8b
mcgranpa <fmcmeans@gmail.com>
2021-01-22 16:21:44 -0600

    Throttling Our Attacks

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,11 +9,17 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
+
 
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
             if (!GetIsInRange())
             {
@@ -23,7 +29,21 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehavior();
                 //print("in range");
+            }
+        }
+
+        private void AttackBehavior()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            if (healthComponent.GetHealth() <= 0.5f) return;
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                // this will trigger the Hit() event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+
             }
         }
 
@@ -45,8 +65,11 @@ namespace RPG.Combat
            // print("cancel");
         }
 
+        // called from animation Unarmed-Attack-L3
         void Hit()
-        { 
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
     }
 }
